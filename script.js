f0a0fff90268ad9f0d7dc16c41d7ebbef162950e
perazaroberto69 <perazaroberto69@gmail.com>
2026-03-10 18:35:12 +0000

    done

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Script started")
+console.log("Script started");
+
+function flipCard(){
+    let card = document.getElementById("cardId");
+    card.style.color = "rgb(79, 82, 161, 0.66)";
+}
