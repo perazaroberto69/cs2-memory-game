4d485fee45d082addd72f87b4ae59c06f419bac6
perazaroberto69 <perazaroberto69@gmail.com>
2026-03-11 18:56:08 +0000

    done

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 console.log("Script started");
 
-function flipCard(){
-    let card = document.getElementById("cardId");
-    card.style.color = "rgb(79, 82, 161, 0.66)";
+let guesses = 0;
+let firstCardId = "";
+let secondCardId = "";
+
+//The parameter helps us use one function for multiple different things "CardId"
+function flipCard(cardId) {
+    let card = document.getElementById(cardId);
+    card.style.color = "rgba(79, 82, 161, 0.66)";
+    
+    if (firstCardId == ""){
+    firstCardId = cardId;
+    } else {
+    secondCardId = cardId;
+    }
+    console.log(firstCardId);
+    console.log(secondCardId);
+    checkForMatch();
+}
+
+function checkForMatch(){
+    let card1 = document.getElementById(firstCardId);
+    let card2 = document.getElementById(secondCardId);
+    console.log(card1);
+    console.log(card2);
+    if (card1.innerText == card7.innerText){
+        console.log("match");
+        card1.style.backgroundColor = "green"
+        card2.style.backgroundColor = "green"
+    } else {
+        card1.style.color = "white";
+        card1.style.backgroundColor = "white";
+        card2.style.color = "white";
+        card2.style.backgroundColor = "white";
+    }
+    firstCardId = "";
+    secondCardId = "";
+
 }
